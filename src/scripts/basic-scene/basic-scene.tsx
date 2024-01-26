26bae54a130f2c78ac0f6a52b652f60fdfe4ed04
rans0000 <rans0000@gmail.com>
2024-01-26 19:02:31 +0530

    configure prettier/eslint

--- src/scripts/basic-scene/basic-scene.tsx
+++ src/scripts/basic-scene/basic-scene.tsx
@@ -1,4 +1,4 @@
-import * as THREE from "three";
+import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -15,8 +15,7 @@ scene.add(cube);
 camera.position.z = 5;
 
 function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
 }
 animate();
-
