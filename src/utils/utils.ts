3d9ac63b691c42cc8009f044e67bd4ad1832572a
rans0000 <rans0000@gmail.com>
2024-09-09 22:28:25 +0530

    refactor styles

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,5 @@
-import RAPIER from '@dimforge/rapier3d-compat';
-import * as THREE from 'three';
+import { World } from '@dimforge/rapier3d-compat';
+import { PerspectiveCamera, Scene, SpotLight, VSMShadowMap, WebGLRenderer } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 function createBitmask(indices: number[]): string {
@@ -18,13 +18,13 @@ export function generateCollisionId(members: number[], filters: number[]): numbe
 export function initBasicScene() {
     // setup the scene
     const gravity = { x: 0, y: -9.81, z: 0 };
-    const world = new RAPIER.World(gravity);
+    const world = new World(gravity);
 
     // setup threejs
-    const scene = new THREE.Scene();
+    const scene = new Scene();
 
     // setup the light
-    const light = new THREE.SpotLight(undefined, Math.PI * 10);
+    const light = new SpotLight(undefined, Math.PI * 10);
     light.position.set(2.5, 5, 5);
     light.angle = Math.PI / 3;
     light.penumbra = 0.5;
@@ -34,19 +34,14 @@ export function initBasicScene() {
     scene.add(light);
 
     // setup the camera
-    const camera = new THREE.PerspectiveCamera(
-        50,
-        window.innerWidth / window.innerHeight,
-        0.1,
-        100
-    );
+    const camera = new PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 100);
     camera.position.set(0, 2, 5);
 
     // setup renderer
-    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    const renderer = new WebGLRenderer({ antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.shadowMap.enabled = true;
-    renderer.shadowMap.type = THREE.VSMShadowMap;
+    renderer.shadowMap.type = VSMShadowMap;
     document.body.appendChild(renderer.domElement);
 
     window.addEventListener('resize', () => {
